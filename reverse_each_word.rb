2b8a2c32fcbe1eb47e70e6dc383c221a815d5ca9
Josh Allen <joshmrallen@gmail.com>
2020-02-28 21:54:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   
   reversed_words = string_array.map {|word| word.reverse}
   
-  reversed_string = reversed.join(" ")
+  reversed_string = reversed_words.join(" ")
   
   return reversed_string
   
